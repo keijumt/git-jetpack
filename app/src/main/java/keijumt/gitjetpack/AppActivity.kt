a5db39ad52c24a16743c07103173432a0daa1dbd
keijumt <keijumt@gmail.com>
2019-06-01 00:57:24 +0900

    Rename MainActivity to AppActivity

--- app/src/main/java/keijumt/gitjetpack/AppActivity.kt
+++ app/src/main/java/keijumt/gitjetpack/AppActivity.kt
@@ -3,10 +3,10 @@ package keijumt.gitjetpack
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class AppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_app)
     }
 }
