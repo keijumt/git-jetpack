788d7b757c5927dd0e0786edbe2760950d50d1a7
keijumt <matsumoto_k@camobile.com>
2019-06-14 17:50:56 +0900

    Change the color of the status bar in night mode

--- app/src/main/java/keijumt/gitjetpack/ui/MainActivity.kt
+++ app/src/main/java/keijumt/gitjetpack/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package keijumt.gitjetpack.ui
 
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -12,6 +17,11 @@ import keijumt.gitjetpack.R
 import keijumt.gitjetpack.databinding.ActivityMainBinding
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    companion object {
+        private const val COLOR_STATUS_BAR_INVISIBLE = Color.TRANSPARENT
+        private const val COLOR_STATUS_BAR_VISIBLE = 0x8a000000.toInt()
+    }
 
     private val navController: NavController by lazy {
         findNavController(R.id.root_nav_host_fragment)
@@ -26,6 +36,16 @@ class MainActivity : DaggerAppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController)
+
+        val isNightMode = isNightMode(this)
+        if (Build.VERSION.SDK_INT >= 23) {
+            window.decorView.systemUiVisibility = if (isNightMode) {
+                0
+            } else {
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
+        window.statusBarColor = ContextCompat.getColor(this, R.color.status_bar)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val config = PageConfiguration.getConfiguration(destination.id)
@@ -50,6 +70,15 @@ class MainActivity : DaggerAppCompatActivity() {
         }
 
         binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    private fun isNightMode(context: Context): Boolean {
+        return when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
+        }
     }
 
     override fun onSupportNavigateUp() = navController.navigateUp()
